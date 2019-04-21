7cbdec7eab5e75c2c83abe0d2affeb94f18b15fc
Kuba Naskrętski <kuba228@gmail.com>
2019-04-21 16:34:44 +0200

    1st heroku upload

--- lnsitedeployone/settings.py
+++ lnsitedeployone/settings.py
@@ -25,7 +25,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
