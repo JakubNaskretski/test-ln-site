7a5ed606ac1966e65550548494f8cbe10a1340cd
Kuba Naskrętski <kuba228@gmail.com>
2019-04-27 15:48:31 +0200

    moved files folder from static

--- mainapp/views.py
+++ mainapp/views.py
@@ -51,16 +51,16 @@ def GotoweRozwiazania(request):
         funcdrinksprod = Product.objects.filter(product_type='Napoje funkcjonalne')
         #TODO: czy filtr może się jakoś automatycznie aktualizowac?
         # syrupprod = Product.objects.filter(product_type='Syropy')
-        # try:
-    if request.method == 'GET':
-        return render(request, "GotoweRozwiazania.html", {'funcdrinksprod':funcdrinksprod})
-    elif request.method == 'POST':
-        text = """<h1>Looks like your web browser tried to POST</h1>"""
-        return HttpResponse(text)
-    # except Exception as e:
-    #     raise Http404("Something went wrong")
-    # except Exception as e:
-    #     raise Http404("Couldnt load products from database")
+        try:
+            if request.method == 'GET':
+                return render(request, "GotoweRozwiazania.html", {'funcdrinksprod':funcdrinksprod})
+            elif request.method == 'POST':
+                text = """<h1>Looks like your web browser tried to POST</h1>"""
+                return HttpResponse(text)
+        except Exception as e:
+            raise Http404("Something went wrong")
+    except Exception as e:
+        raise Http404("Couldnt load products from database")
 
 
 def Kontakt(request):
