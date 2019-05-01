b70c4acb4904aeb47352dd23dbb8393d8742fdb5
Kuba Naskrętski <kuba228@gmail.com>
2019-05-01 21:49:43 +0200

    adding slides for main page, js file

---
+++ staticfiles/js/stronaglowna.42b1d62c3760.42b1d62c3760.js
@@ -0,0 +1,31 @@
+var i = 0; //startpoint
+var images = [];
+var time = 5000;
+const contentList = document.querySelectorAll('.content__text');
+
+function hideElements() {
+  Array.from(contentList).map(item => item.style.display = 'none');
+}
+
+
+// Image list
+images[0] = document.getElementsByClassName("slajd1");
+images[1] = document.getElementsByClassName("slajd2");
+images[2] = document.getElementsByClassName("slajd3");
+
+//change image
+function changeImg(){
+  hideElements();
+  document.getElementById("images" + i).style.display = "block";
+
+  if(i < images.length - 1){
+    i++;
+  }else {
+    i = 0;
+  }
+
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
