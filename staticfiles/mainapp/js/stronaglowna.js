6a08e2de32ea37669edc60d0d0473829e24c2bd7
Kuba Naskrętski <kuba228@gmail.com>
2019-05-01 21:25:48 +0200

    adding slides for main page, js file

--- staticfiles/mainapp/js/stronaglowna.js
+++ staticfiles/mainapp/js/stronaglowna.js
@@ -2,14 +2,17 @@ var i = 0; //startpoint
 var images = [];
 var time = 1000;
 
+// const contentList = document.querySelectorAll('.content__text');
+
+
 // Image list
-images[0] = 'slajd1.jpg';
-images[1] = 'slajd2.jpg';
-images[2] = 'slajd3.png';
+images[0] = document.getElementsByClassName("slajd1");
+images[1] = document.getElementsByClassName("slajd2");
+images[2] = document.getElementsByClassName("slajd3");
 
 //change image
 function changeImg(){
-  document.getElementById("slides").src = images [i];
+  document.getElementById("slides").src = images[i].style.display = "block";
 
   if(i < images.length - 1){
     i++;
