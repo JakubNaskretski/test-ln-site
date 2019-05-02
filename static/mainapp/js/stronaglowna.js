46eeb763a14d4ccd76153a403d9367aa0b1a470e
Kuba Naskrętski <kuba228@gmail.com>
2019-05-02 19:39:17 +0200

    testing new slideshow

--- static/mainapp/js/stronaglowna.js
+++ static/mainapp/js/stronaglowna.js
@@ -1,16 +1,17 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
-//
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-//
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-//
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// // on click slides
 // function showSlides(n) {
 //   var i;
 //   var slides = document.getElementsByClassName("mySlides");
@@ -27,47 +28,18 @@
 //   dots[slideIndex-1].className += " active";
 // }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// automatic Slideshow
+var slideIndex = 0;
+showSlides();
 
-
-function plusSlides(n) {
-clearInterval(myTimer);
-this.showSLides(slideIndex +=n);
-if (n = -1){
-	myTimer = setInterval(() => {this.plusSlides(n + 2);}, 4000);
-    } else {
-   	myTimer = setInterval(() => {this.plusSlides(n + 1);}, 4000);
-    }
-   }
-
-function currentSlide(n) {
-  clearInterval(myTimer);
-  this.showSlides(slideIndex = n);
-  if (n = -1) {
-  	myTimer = setInterval(() => {this.plusSlides(n + 2);}, 4000);
-    } else {
-    	myTimer = setInterval(() => {this.plusSlides(n + 1);}, 4000);
-       }
-}
-
-function componentDidMount = () {
-	this.showSLides(slideIndex);
-    myTimer = setInterval(() => {this.plusSlides(1);}, 4000);
-   }
-
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
---
+++ staticfiles/js/stronaglowna.67c0b02c2104.67c0b02c2104.js
@@ -0,0 +1,73 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+clearInterval(myTimer);
+this.showSLides(slideIndex +=n);
+if (n = -1){
+	myTimer = setInterval(() => {this.plusSlides(n + 2);}, 4000);
+    } else {
+   	myTimer = setInterval(() => {this.plusSlides(n + 1);}, 4000);
+    }
+   }
+
+function currentSlide(n) {
+  clearInterval(myTimer);
+  this.showSlides(slideIndex = n);
+  if (n = -1) {
+  	myTimer = setInterval(() => {this.plusSlides(n + 2);}, 4000);
+    } else {
+    	myTimer = setInterval(() => {this.plusSlides(n + 1);}, 4000);
+       }
+}
+
+function componentDidMount = () {
+	this.showSLides(slideIndex);
+    myTimer = setInterval(() => {this.plusSlides(1);}, 4000);
+   }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
