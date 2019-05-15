9b7b34485c283baf8bcb5bf174307e89e2e4f367
Kuba Naskrętski <kuba228@gmail.com>
2019-05-15 22:43:11 +0200

    blocking js on main page

--- static/mainapp/js/stronaglowna.js
+++ static/mainapp/js/stronaglowna.js
@@ -42,7 +42,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 500000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
 // 5000
